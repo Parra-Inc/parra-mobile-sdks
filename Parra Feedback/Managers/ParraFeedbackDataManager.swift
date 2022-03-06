340072d5ab4cb196aaafeb253691df8d04f3ce2a
Michael MacCallum <micksmaccallum@gmail.com>
2022-03-05 19:17:38 -0500

    removed dead code

--- Parra Feedback/Managers/ParraFeedbackDataManager.swift
+++ Parra Feedback/Managers/ParraFeedbackDataManager.swift
@@ -89,44 +89,6 @@ class ParraFeedbackDataManager {
         cardStorage.updateCards(cardItems: cards)
     }
     
-    /// Depending on the value of the replace arguement, either replaces the existing cards with the supplied ones
-    /// or attempts to merge the supplied cards with the current cards, attempting to keep order similar.
-//    func storeCards(cards: [CardItem], replace: Bool = true) {
-//        queue.async {
-//            let cardDataList = cards.map { $0.data }
-//
-//            if replace {
-//                self.cardDataMap = self.cardDataMapFromList(
-//                    cardDataList: cardDataList
-//                )
-//            } else {
-//
-//                for (index, data) in cardDataList.lazy.enumerated() {
-//                    if let existing = self.cardDataMap[data.id] {
-//                        // If there is already a card with this id, update the card but leave it in the same
-//                        // place in the list it existed in previously.
-//                        self.cardDataMap[data.id] = (existing.index, data)
-//                    } else {
-//                        // This is a new card that we weren't tracking. Use the index within the new list
-//                        // as the index for the card in the main list. This is a best guess and will just be used
-//                        // during sorting later so it doesn't need to be exact.
-//                        self.cardDataMap[data.id] = (index, data)
-//                    }
-//                }
-//            }
-//
-//            self.writeCardData()
-//        }
-//    }
-        
-//    func currentData<T: ReadWriteable>() async -> [T] {
-//        return await withCheckedContinuation { continuation in
-//            queue.async {
-//                continuation.resume(returning: [])
-//            }
-//        }
-//    }
-    
     func loadData() async throws {
         let _ = await [
             credentialStorage.loadData(),
@@ -140,37 +102,7 @@ class ParraFeedbackDataManager {
         // TODO: should log an event to the current session.
         // TODO: Should be called in addition to updating answers.
     }
-        
-//    private func loadCardData() async throws -> FastAccessListMap<CardItemData> {
-//        let fileManager = FileManager.default
-//        let url = kParraCardDataPath
-//
-//        guard fileManager.fileExists(atPath: url.path) else {
-//            return [:]
-//        }
-//
-//        let data = try Data(contentsOf: url)
-//
-//        return cardDataMapFromList(
-//            cardDataList: try JSONDecoder.parraDecoder.decode([CardItemData].self, from: data)
-//        )
-//    }
-//
-//    private func loadAnswers() async throws -> [String: Answer] {
-//        let fileManager = FileManager.default
-//        let url = kParraAnswerDataPath
-//
-//        guard fileManager.fileExists(atPath: url.path) else {
-//            return [:]
-//        }
-//
-//        let data = try Data(contentsOf: url)
-//
-//        return answersMapFromList(
-//            answers: try JSONDecoder.parraDecoder.decode([Answer].self, from: data)
-//        )
-//    }
-    
+
     func cardDataMapFromList(cardDataList: [CardItemData]) -> FastAccessListMap<CardItemData> {
         return cardDataList.lazy.enumerated().reduce([:]) { partialResult, obj in
             var result = partialResult
@@ -198,18 +130,6 @@ class ParraFeedbackDataManager {
             return result
         }
     }
-    
-//    func answersListFromMap(answerDataMap: [String: Answer]) -> [Answer] {
-//        return Array(answerDataMap.values)
-//    }
-    
-//    private func writeCardData() {
-//
-//    }
-//
-//    private func writeAnswers() {
-//
-//    }
 }
 
 extension CardItemData: Identifiable {
