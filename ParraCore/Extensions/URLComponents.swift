80942e7c50100a3c5f0ec5cab70d8270e3c88eea
Mick MacCallum <micksmaccallum@gmail.com>
2022-09-18 12:42:00 -0400

    added helper to convert dictionaries to query items

---
+++ ParraCore/Extensions/URLComponents.swift
@@ -0,0 +1,17 @@
+//
+//  URLComponents.swift
+//  ParraCore
+//
+//  Created by Mick MacCallum on 9/18/22.
+//  Copyright © 2022 Parra, Inc. All rights reserved.
+//
+
+import Foundation
+
+extension URLComponents {
+    mutating func setQueryItems(with parameters: [String: String]) {
+        queryItems = parameters.map {
+            URLQueryItem(name: $0.key, value: $0.value)
+        }
+    }
+}
