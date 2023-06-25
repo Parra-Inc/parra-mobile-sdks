7c94cc7725c85bfe2829983f9242d9fc2dbbdea8
Mick MacCallum <micksmaccallum@gmail.com>
2023-06-25 17:22:42 -0400

    Split up logger helpers, added tests

--- Parra/Logger/ParraDefaultLogger.swift
+++ Parra/Logger/ParraDefaultLogger.swift
@@ -72,15 +72,19 @@ internal class ParraDefaultLogger: ParraLogger {
         case .string(let messageProvider):
             baseMessage = messageProvider()
         case .error(let errorProvider):
-            baseMessage = extractMessage(from: errorProvider())
+            baseMessage = LoggerHelpers.extractMessage(
+                from: errorProvider()
+            )
         }
 
         let timestamp = ParraDefaultLogger.timestampFormatter.string(from: Date())
         let queue = currentThread.queueName
-        let (module, file) = splitFileId(fileId: fileID)
+        let (module, file, _) = LoggerHelpers.splitFileId(fileId: fileID)
         var extraWithAdditions = extra ?? [:]
         if let extraError {
-            extraWithAdditions["errorDescription"] = extractMessage(from: extraError)
+            extraWithAdditions["errorDescription"] = LoggerHelpers.extractMessage(
+                from: extraError
+            )
         }
 
 #if DEBUG
@@ -116,7 +120,11 @@ internal class ParraDefaultLogger: ParraLogger {
         }
 
         if loggerConfig.printCallsite {
-            let formattedLocation = createFormattedLocation(fileID: fileID, function: function, line: line)
+            let formattedLocation = LoggerHelpers.createFormattedLocation(
+                fileID: fileID,
+                function: function,
+                line: line
+            )
             formattedMessage = "\(formattedMarkers) \(formattedLocation) \(baseMessage)"
         }
 
@@ -149,65 +157,7 @@ internal class ParraDefaultLogger: ParraLogger {
 
         // TODO: Would capturing other info from ProcessInfo.processInfo.environment help with symbolication?
 
-        Parra.logAnalyticsEvent(ParraSessionEventType._Internal.log, params: params)
+        Parra.logEvent(ParraSessionEventType._Internal.log, params: params)
 #endif
-    }
-
-    private func extractMessage(from error: Error) -> String {
-        if let parraError = error as? ParraError {
-            return parraError.errorDescription
-        } else {
-            // Error is always bridged to NSError, can't downcast to check.
-            if type(of: error) is NSError.Type {
-                let nsError = error as NSError
-
-                return "Error domain: \(nsError.domain), code: \(nsError.code), description: \(nsError.localizedDescription)"
-            } else {
-                return String(reflecting: error)
-            }
-        }
-    }
-
-    private func splitFileId(fileId: String) -> (module: String, fileName: String) {
-        let initialSplit = {
-            let parts = fileId.split(separator: "/")
-
-            if parts.count == 0 {
-                return ("Unknown", "Unknown")
-            } else if parts.count == 1 {
-                return ("Unknown", String(parts[0]))
-            } else if parts.count == 2 {
-                return (String(parts[0]), String(parts[1]))
-            } else {
-                return (String(parts[0]), parts.dropFirst(1).joined(separator: "/"))
-            }
-        }
-
-        let (module, fileName) = initialSplit()
-
-        let fileParts = fileName.split(separator: ".")
-        if fileParts.count == 1 {
-            return (module, fileName)
-        } else {
-            return (module, fileParts.dropLast(1).joined(separator: "."))
-        }
-    }
-
-    private func createFormattedLocation(fileID: String, function: String, line: Int) -> String {
-        let file: String
-        if let extIndex = fileID.lastIndex(of: ".") {
-            file = String(fileID[..<extIndex])
-        } else {
-            file = fileID
-        }
-
-        let functionName: String
-        if let parenIndex = function.firstIndex(of: "(") {
-            functionName = String(function[..<parenIndex])
-        } else {
-            functionName = function
-        }
-
-        return "\(file).\(functionName)#\(line)"
     }
 }
---
+++ ParraTests/Util/LoggerHelpersTests.swift
@@ -0,0 +1,158 @@
+//
+//  LoggerHelpersTests.swift
+//  ParraTests
+//
+//  Created by Mick MacCallum on 6/25/23.
+//  Copyright © 2023 Parra, Inc. All rights reserved.
+//
+
+import XCTest
+@testable import Parra
+
+fileprivate enum LoggerTestError: Error {
+    case exception
+    case uniquelyNamedErrorCase
+}
+
+final class LoggerHelpersTests: XCTestCase {
+    // MARK: - extractMessage
+
+    func testExtractErrorMessageReturnsCustomParraErrors() {
+        let extraMessage = "something"
+        let error = ParraError.authenticationFailed(extraMessage)
+
+        let result = LoggerHelpers.extractMessage(from: error)
+
+        XCTAssertEqual(result, error.errorDescription)
+    }
+
+    func testExtractErrorMessageFromNsErrors() {
+        let domain = "testDomain"
+        let localizedDescription = "error description wooo"
+        let code = 420
+
+        let error = NSError(
+            domain: domain,
+            code: code,
+            userInfo: [
+                NSLocalizedDescriptionKey: localizedDescription
+            ]
+        )
+
+        let result = LoggerHelpers.extractMessage(from: error)
+
+        XCTAssertTrue(result.contains(domain))
+        XCTAssertTrue(result.contains(localizedDescription))
+        XCTAssertTrue(result.contains(String(code)))
+    }
+
+    func testExtractErrorMessageFromErrorProtocolTypes() {
+        let error = LoggerTestError.uniquelyNamedErrorCase
+        let result = LoggerHelpers.extractMessage(from: error)
+
+        XCTAssertTrue(result.contains("LoggerTestError.uniquelyNamedErrorCase"))
+    }
+
+    // MARK: splitFileId
+
+    func testSplitExpectedFileId() {
+        splitAndAssertEqual(
+            fileId: "Parra/LoggerHelpers.swift",
+            expectedModule: "Parra",
+            expectedFileName: "LoggerHelpers",
+            expectedExtension: "swift"
+        )
+    }
+
+    func testSplitEmptyFileId() {
+        splitAndAssertEqual(
+            fileId: "",
+            expectedModule: "Unknown",
+            expectedFileName: "Unknown",
+            expectedExtension: ""
+        )
+    }
+
+    func testSplitMissingModuleFileId() {
+        splitAndAssertEqual(
+            fileId: "LoggerHelpers.swift",
+            expectedModule: "Unknown",
+            expectedFileName: "LoggerHelpers",
+            expectedExtension: "swift"
+        )
+    }
+
+    func testSplitExtraFilePathComponentsFileId() {
+        splitAndAssertEqual(
+            fileId: "Parra/Intermediate/Folders/LoggerHelpers.swift",
+            expectedModule: "Parra",
+            expectedFileName: "Intermediate/Folders/LoggerHelpers",
+            expectedExtension: "swift"
+        )
+    }
+
+    func testSplitMissingExtensionFileId() {
+        splitAndAssertEqual(
+            fileId: "Parra/LoggerHelpers",
+            expectedModule: "Parra",
+            expectedFileName: "LoggerHelpers",
+            expectedExtension: ""
+        )
+    }
+
+    func testSplitMultipleFileExtensionsFileId() {
+        splitAndAssertEqual(
+            fileId: "Parra/LoggerHelpers.swift.gz",
+            expectedModule: "Parra",
+            expectedFileName: "LoggerHelpers",
+            expectedExtension: "swift.gz"
+        )
+    }
+
+    // MARK: - createFormattedLocation
+
+    func testFormatsLogLocation() {
+        let slug = LoggerHelpers.createFormattedLocation(
+            fileID: "Parra/LoggerHelpers.swift",
+            function: "createFormattedLocation(fileID:function:line:)",
+            line: 69
+        )
+
+        XCTAssertEqual(slug, "Parra/LoggerHelpers.createFormattedLocation#69")
+    }
+
+    func testFormatsLogLocationMissingFileExtension() {
+        let slug = LoggerHelpers.createFormattedLocation(
+            fileID: "Parra/LoggerHelpers",
+            function: "createFormattedLocation(fileID:function:line:)",
+            line: 69
+        )
+
+        XCTAssertEqual(slug, "Parra/LoggerHelpers.createFormattedLocation#69")
+    }
+
+    func testFormatsLogLocationForFunctionsWithoutParams() {
+        let slug = LoggerHelpers.createFormattedLocation(
+            fileID: "Parra/LoggerHelpers.swift",
+            function: "createFormattedLocation",
+            line: 69
+        )
+
+        XCTAssertEqual(slug, "Parra/LoggerHelpers.createFormattedLocation#69")
+    }
+
+    private func splitAndAssertEqual(
+        fileId: String,
+        expectedModule: String,
+        expectedFileName: String,
+        expectedExtension: String
+    ) {
+        let (module, fileName, ext) = LoggerHelpers.splitFileId(
+            fileId: fileId
+        )
+
+        XCTAssertTrue(module == expectedModule)
+        XCTAssertTrue(fileName == expectedFileName)
+        XCTAssertTrue(ext == expectedExtension)
+    }
+}
