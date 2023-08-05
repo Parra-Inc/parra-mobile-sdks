5b0b4b7b3db94f103b7b65fa85ebc9a91458baa8
Mick MacCallum <micksmaccallum@gmail.com>
2023-08-05 12:13:19 -0400

    Fixed dict convertible naming

---
+++ Parra/Extensions/URLRequest+ParraDictionaryConvertible.swift
@@ -0,0 +1,15 @@
+//
+//  URLRequest+ParraDictionaryConvertible.swift
+//  Parra
+//
+//  Created by Mick MacCallum on 8/5/23.
+//  Copyright © 2023 Parra, Inc. All rights reserved.
+//
+
+import Foundation
+
+extension URLRequest: ParraDictionaryConvertible {
+    var dictionary: [String: Any] {
+        return [:]
+    }
+}
