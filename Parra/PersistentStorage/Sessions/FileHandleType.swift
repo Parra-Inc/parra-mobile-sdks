f84b3c57a528f73df36ce44e0726d3c37a23c73b
Mick MacCallum <micksmaccallum@gmail.com>
2023-08-27 14:35:18 -0400

    Added file handle type enum for session storage

---
+++ Parra/PersistentStorage/Sessions/FileHandleType.swift
@@ -0,0 +1,14 @@
+//
+//  FileHandleType.swift
+//  Parra
+//
+//  Created by Mick MacCallum on 8/27/23.
+//  Copyright © 2023 Parra, Inc. All rights reserved.
+//
+
+import Foundation
+
+internal enum FileHandleType {
+    case session
+    case events
+}
