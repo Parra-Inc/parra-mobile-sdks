f6ef7e756a4177aa501f8ffcbc489840fda3321d
Mick MacCallum <micksmaccallum@gmail.com>
2023-11-04 12:54:06 -0400

    code signing

--- scripts/ci/build-for-testing.sh
+++ scripts/ci/build-for-testing.sh
@@ -11,6 +11,9 @@ build() {
             -scheme "$PARRA_TEST_SCHEME_NAME" \
             -configuration "$PARRA_TEST_CONFIGURATION" \
             -destination "$PARRA_TEST_DESTINATION" \
+            -authenticationKeyPath "$PARRA_ASC_API_KEY_PATH" \
+            -authenticationKeyID "ASC_API_KEY_ID" \
+            -authenticationKeyIssuerID "ASC_API_ISSUER_ID" \
             -derivedDataPath "$PARRA_TEST_DERIVED_DATA_DIRECTORY" |
         tee buildlog |
             xcbeautify --is-ci --junit-report-filename artifacts/junit-results.xml
