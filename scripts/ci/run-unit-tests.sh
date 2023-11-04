ab369f3ff52a0112e799ec5f4ed9ad3329444299
Mick MacCallum <micksmaccallum@gmail.com>
2023-11-04 13:48:40 -0400

    first attempt at running tests

--- scripts/ci/run-unit-tests.sh
+++ scripts/ci/run-unit-tests.sh
@@ -2,23 +2,26 @@
 
 set -xo pipefail
 
-export CONFIGURATION_BUILD_DIR="$DERIVED_DATA_DIRECTORY"
-
-DATADIR=derivedData
-OUTDIR=output
-DERIVED_DATA=(-derivedDataPath "$DATADIR")
-OUTPUT=(-resultBundlePath "$OUTDIR")
+CONFIGURATION_BUILD_DIR="$PARRA_TEST_DERIVED_DATA_DIRECTORY"
 
 test() {
-    rm -rf "$OUTDIR"
-    xcodebuild -scheme Tests -project ./MyApp.xcodeproj -destination 'platform=iOS Simulator,name=iPhone 6' ${DERIVED_DATA[@]} ${OUTPUT[@]} test-without-building | bundle exec xcpretty --color
+    rm -rf "$PARRA_TEST_OUTPUT_DIRECTORY"
+
+    xcodebuild test-without-building \
+        -project "$PARRA_TEST_PROJECT_NAME" \
+        -scheme "$PARRA_TEST_SCHEME_NAME" \
+        -configuration "$PARRA_TEST_CONFIGURATION" \
+        -destination "$PARRA_TEST_DESTINATION" \
+        -derivedDataPath "$PARRA_TEST_DERIVED_DATA_DIRECTORY" \
+        -resultBundlePath "$PARRA_TEST_OUTPUT_DIRECTORY" |
+        xcbeautify --is-ci --junit-report-filename artifacts/junit-results.xml
     RESULT=$?
 }
 
 for run in {1..5}; do
     test
 
-    ! grep --max-count=1 'Early unexpected exit, operation never finished bootstrapping' "$OUTDIR"/TestSummaries.plist && break
+    # ! grep --max-count=1 'Early unexpected exit, operation never finished bootstrapping' "$OUTDIR"/TestSummaries.plist && break
 done
 
 # ./scripts/plist2junit.rb "$OUTDIR"/TestSummaries.plist >"$OUTDIR"/report.junit
