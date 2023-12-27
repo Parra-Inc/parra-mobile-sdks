00164731a81401a9298d52d77831b1ffa163f0ec
Mick MacCallum <micksmaccallum@gmail.com>
2023-12-27 18:40:00 -0500

    passing vars between jobs

--- scripts/ci/install-brew-dependencies.sh
+++ scripts/ci/install-brew-dependencies.sh
@@ -1,5 +1,26 @@
 #! /bin/bash
 
+echo "Installing Homebrew dependencies..."
+
+# Don't check for and install updates for taps on every brew command.
 export HOMEBREW_NO_AUTO_UPDATE=1
 
+# Speed up by skipping cleanup tasks after install.
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+
+
+echo "Installing jq..."
+# Used for parsing JSON in CLI tools. This is deliberately used instead of modern equivalents like
+# fx, because it's available on macOS executors in CircleCI by default, and even when missing installs
+# much more quickly.
+brew install jq
+
+
+echo "Installing applesimutils..."
+# Used for scripts that query for specific simulators via simctl.
+brew tap wix/brew && brew install applesimutils
+
+
+echo "Installing xcbeautify..."
+# Used for pretty printing xcodebuild output.
 brew install xcbeautify
---
+++ scripts/ci/preboot-simulator.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# This is used instead of the macOS ORB's preboot-simulator step, since it takes too long to return.
+
+SCRIPT_DIR=$(dirname "$0")
+
+deviceId=$("$SCRIPT_DIR/obtain-simulator-device-id.sh" "$PARRA_TEST_DEVICE_NAME" "$PARRA_TEST_DEVICE_OS_VERSION") 2> echo
+
+echo "Booting simulator with device ID: $deviceId"
+
+# Boot the device but don't wait for it to finish. We want to get through other preparations while it's booting
+# and there will be another task later that waits for it to finish booting before starting tests.
+xcrun simctl boot $deviceId --arch=arm64 & 
+
+# Store the device ID in an environment variable so it can be used by other steps.
+export PARRA_TEST_DEVICE_UDID="$deviceId"
