113618e405873d489efb7c828bb965205cba6ea9
Mick MacCallum <micksmaccallum@gmail.com>
2023-12-27 20:21:15 -0500

    cleanup

--- scripts/ci/disable-simulator-hardware-keyboard.sh
+++ scripts/ci/disable-simulator-hardware-keyboard.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 
+echo "Killing simulator"
+set +e
+
 killall Simulator
+
+echo "Disabling hardware keyboard"
 defaults write com.apple.iphonesimulator ConnectHardwareKeyboard -bool false
