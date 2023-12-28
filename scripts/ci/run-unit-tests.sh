331d4a4f725656a363aad288cff830b2ee1c0487
Mick MacCallum <micksmaccallum@gmail.com>
2023-12-27 19:17:17 -0500

    hmm

--- scripts/ci/run-unit-tests.sh
+++ scripts/ci/run-unit-tests.sh
@@ -13,8 +13,11 @@ test() {
         -configuration "$PARRA_TEST_CONFIGURATION" \
         -destination "$PARRA_TEST_DESTINATION" \
         -derivedDataPath "$PARRA_TEST_DERIVED_DATA_DIRECTORY" \
-        -resultBundlePath "$PARRA_TEST_OUTPUT_DIRECTORY" |
-        xcbeautify --is-ci --junit-report-filename artifacts/junit-results.xml
+        -resultBundlePath "$PARRA_TEST_OUTPUT_DIRECTORY" \
+        -parallel-testing-enabled yes \
+        # -parallel-testing-worker-count 2 \
+        -maximum-parallel-testing-workers 4 \
+        | xcbeautify --is-ci --junit-report-filename artifacts/junit-results.xml
     RESULT=$?
 }
 
