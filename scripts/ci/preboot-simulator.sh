278f1615f77e229be66cf2fbd9e804099ba2d65d
Mick MacCallum <micksmaccallum@gmail.com>
2023-12-27 19:24:06 -0500

    idk

--- scripts/ci/preboot-simulator.sh
+++ scripts/ci/preboot-simulator.sh
@@ -2,13 +2,14 @@
 
 # This is used instead of the macOS ORB's preboot-simulator step, since it takes too long to return.
 
-deviceId=`/tmp/workspace/scripts/ci/obtain-simulator-device-id.sh "$PARRA_TEST_DEVICE_NAME" "$PARRA_TEST_DEVICE_OS_VERSION" 2> echo`
+deviceId=`/tmp/workspace/scripts/ci/obtain-simulator-device-id.sh "$PARRA_TEST_DEVICE_NAME" "$PARRA_TEST_DEVICE_OS_VERSION"`
 
 echo "Booting simulator with device ID: $deviceId"
+
+# Store the device ID in an environment variable so it can be used by other steps.
+export PARRA_TEST_DEVICE_UDID="$deviceId"
 
 # Boot the device but don't wait for it to finish. We want to get through other preparations while it's booting
 # and there will be another task later that waits for it to finish booting before starting tests.
 xcrun simctl boot $deviceId --arch=arm64 & 
 
-# Store the device ID in an environment variable so it can be used by other steps.
-export PARRA_TEST_DEVICE_UDID="$deviceId"
