56871a0431091d05338e4d3617d046f145a9d19d
Mick MacCallum <micksmaccallum@gmail.com>
2024-03-11 15:30:35 -0400

    Move ScrollToTopView

--- Parra/Containers/Widgets/Roadmap/Public/RoadmapWidget.swift
+++ Parra/Containers/Widgets/Roadmap/Public/RoadmapWidget.swift
@@ -8,27 +8,6 @@
 
 import SwiftUI
 
-struct ScrollToTopView<T>: View where T: Equatable {
-    let reader: ScrollViewProxy
-    let animateChanges: Bool
-
-    @Binding var scrollOnChange: T
-
-    var body: some View {
-        EmptyView()
-            .id("topScrollPoint")
-            .onChange(of: scrollOnChange) { _, _ in
-                if animateChanges {
-                    withAnimation {
-                        reader.scrollTo("topScrollPoint", anchor: .bottom)
-                    }
-                } else {
-                    reader.scrollTo("topScrollPoint", anchor: .bottom)
-                }
-            }
-    }
-}
-
 public struct RoadmapWidget: Container {
     // MARK: - Lifecycle
 
--- Parra/Containers/Widgets/Views/ScrollToTopView.swift
+++ Parra/Containers/Widgets/Views/ScrollToTopView.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2024 Parra, Inc. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+struct ScrollToTopView<T>: View where T: Equatable {
+    let reader: ScrollViewProxy
+    let animateChanges: Bool
+
+    @Binding var scrollOnChange: T
+
+    var body: some View {
+        EmptyView()
+            .id("topScrollPoint")
+            .onChange(of: scrollOnChange) { _, _ in
+                if animateChanges {
+                    withAnimation {
+                        reader.scrollTo("topScrollPoint", anchor: .bottom)
+                    }
+                } else {
+                    reader.scrollTo("topScrollPoint", anchor: .bottom)
+                }
+            }
+    }
+}
