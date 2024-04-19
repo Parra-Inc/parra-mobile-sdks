89f250714d1b014737c542132517bceb1cef6e62
Mick MacCallum <micksmaccallum@gmail.com>
2024-04-18 21:56:29 -0400

    Added empty profile tab to sample

---
+++ Sample/ProfileTab.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileTab.swift
+//  Sample
+//
+//  Created by Mick MacCallum on 4/18/24.
+//  Copyright © 2024 Parra, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileTab: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
+
+#Preview {
+    ProfileTab()
+}
