6ab689c1595483d626446e8a103f896c9e80aee0
Mick MacCallum <micksmaccallum@gmail.com>
2024-04-22 12:04:41 -0400

    Remove old auth types from sample app

--- Sample/SampleApp.swift
+++ Sample/SampleApp.swift
@@ -11,8 +11,6 @@ import SwiftUI
 
 @main
 final class SampleApp: ParraApp<ParraAppDelegate, ParraSceneDelegate> {
-    // MARK: - Lifecycle
-
     required init() {
         super.init()
 
@@ -25,61 +23,5 @@ final class SampleApp: ParraApp<ParraAppDelegate, ParraSceneDelegate> {
                 }
             }
         )
-    }
-
-    // MARK: - Internal
-
-    /// An authentication provider to use when running in DEBUG mode. If you're
-    /// working on integrating Parra into your app, this is generally the auth
-    /// flow that you should be implementing for both DEBUG and RELEASE
-    /// environments. This authentication method relies on an OAuth flow with
-    /// your backend to create a Parra access token for a given user.
-    func debugAuthenticationMethod() -> ParraAuthType {
-        return .custom {
-            let myAppAccessToken = Parra.Demo.demoUserId
-
-            var request = URLRequest(
-                // Replace this with your Parra access token generation endpoint
-                url: URL(
-                    string: "http://localhost:8080/v1/parra/auth/token"
-                )!
-            )
-
-            request.httpMethod = "POST"
-            // Replace this with your app's way of authenticating users
-            request.setValue(
-                "Bearer \(myAppAccessToken)",
-                forHTTPHeaderField: "Authorization"
-            )
-
-            let (data, _) = try await URLSession.shared.data(for: request)
-            let response = try JSONDecoder().decode(
-                [String: String].self,
-                from: data
-            )
-
-            return response["access_token"]!
-        }
-    }
-
-    /// An authentication provider used for the beta app for the Parra iOS SDK
-    /// Demo. This app uses public key authentication, which is not preferred
-    /// but provides an example of how it can be achieved and prevents the demo
-    /// app from requiring a backend to authenticate with.
-    func betaAuthenticationMethod() -> ParraAuthType {
-        return .public(
-            apiKeyId: Parra.Demo.apiKeyId,
-            userIdProvider: {
-                return Parra.Demo.demoUserId
-            }
-        )
-    }
-
-    func authenticationMethod() -> ParraAuthType {
-        #if DEBUG
-        return debugAuthenticationMethod()
-        #else
-        return betaAuthenticationMethod()
-        #endif
     }
 }
