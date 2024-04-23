b4b7dca8890e69ffeeb88ff3288af499f5b3f482
Mick MacCallum <micksmaccallum@gmail.com>
2024-04-23 10:42:44 -0400

    helper for focusing text inputs

---
+++ Sources/Parra/Containers/Views/Extensions/View+focusNextField.swift
@@ -0,0 +1,71 @@
+//
+//  View+focusNextField.swift
+//  Parra
+//
+//  Created by Mick MacCallum on 4/23/24.
+//  Copyright © 2024 Parra, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+extension View {
+    /// Focuses next field in sequence, from the given `FocusState`.
+    /// Requires a currently active focus state and a next field available in the sequence.
+    ///
+    /// Example usage:
+    /// ```
+    /// .onSubmit { self.focusNextField($focusedField) }
+    /// ```
+    /// Given that `focusField` is an enum that represents the focusable fields. For example:
+    /// ```
+    /// @FocusState private var focusedField: Field?
+    /// enum Field: Int, Hashable {
+    ///    case name
+    ///    case country
+    ///    case city
+    /// }
+    /// ```
+    func focusNextField<
+        F: RawRepresentable
+    >(
+        _ field: FocusState<F?>.Binding
+    ) where F.RawValue == Int {
+        guard let currentValue = field.wrappedValue else {
+            return
+        }
+        let nextValue = currentValue.rawValue + 1
+        if let newValue = F(rawValue: nextValue) {
+            field.wrappedValue = newValue
+        }
+    }
+
+    /// Focuses previous field in sequence, from the given `FocusState`.
+    /// Requires a currently active focus state and a previous field available in the sequence.
+    ///
+    /// Example usage:
+    /// ```
+    /// .onSubmit { self.focusNextField($focusedField) }
+    /// ```
+    /// Given that `focusField` is an enum that represents the focusable fields. For example:
+    /// ```
+    /// @FocusState private var focusedField: Field?
+    /// enum Field: Int, Hashable {
+    ///    case name
+    ///    case country
+    ///    case city
+    /// }
+    /// ```
+    func focusPreviousField<
+        F: RawRepresentable
+    >(
+        _ field: FocusState<F?>.Binding
+    ) where F.RawValue == Int {
+        guard let currentValue = field.wrappedValue else {
+            return
+        }
+        let nextValue = currentValue.rawValue - 1
+        if let newValue = F(rawValue: nextValue) {
+            field.wrappedValue = newValue
+        }
+    }
+}
