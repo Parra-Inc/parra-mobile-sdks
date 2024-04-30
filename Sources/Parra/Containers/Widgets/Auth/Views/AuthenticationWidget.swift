8f0b75bd6ae63d7daf98c03ea49147a137253f16
Mick MacCallum <micksmaccallum@gmail.com>
2024-04-30 12:53:13 -0400

    Split out email password view

--- Sources/Parra/Containers/Widgets/Auth/Views/AuthenticationWidget.swift
+++ Sources/Parra/Containers/Widgets/Auth/Views/AuthenticationWidget.swift
@@ -29,56 +29,77 @@ public struct AuthenticationWidget: Container {
     // MARK: - Public
 
     public var body: some View {
+        NavigationStack(path: $navigationState.navigationPath) {
+            mainView
+        }
+        .navigationBarTitleDisplayMode(.inline)
+    }
+
+    // MARK: - Internal
+
+    let localBuilderConfig: AuthenticationWidgetBuilderConfig
+    let componentFactory: ComponentFactory
+    @StateObject var contentObserver: ContentObserver
+    let config: ParraAuthConfig
+    let style: AuthenticationWidgetStyle
+
+    @EnvironmentObject var themeObserver: ParraThemeObserver
+
+    @ViewBuilder var mainView: some View {
         let content = contentObserver.content
 
         VStack(spacing: 0) {
-            VStack(spacing: 0) {
-                Spacer()
+            Spacer()
 
-                VStack(spacing: 0) {
-                    HStack(spacing: 0) {
-                        withContent(content: content.icon) { content in
-                            ImageComponent(
-                                content: content,
-                                attributes: ImageAttributes(
-                                    frame: .fixed(
-                                        FixedFrameAttributes(
-                                            width: 60,
-                                            height: 60
-                                        )
+            VStack(spacing: 0) {
+                HStack(spacing: 0) {
+                    withContent(content: content.icon) { content in
+                        ImageComponent(
+                            content: content,
+                            attributes: ImageAttributes(
+                                frame: .fixed(
+                                    FixedFrameAttributes(
+                                        width: 60,
+                                        height: 60
                                     )
                                 )
                             )
-                        }
+                        )
+                    }
 
-                        componentFactory.buildLabel(
-                            config: config.title,
-                            content: content.title,
-                            suppliedBuilder: localBuilderConfig.title,
-                            localAttributes: LabelAttributes(
-                                padding: .padding(
-                                    leading: content.icon == nil ? 0 : 6
-                                )
+                    componentFactory.buildLabel(
+                        config: config.title,
+                        content: content.title,
+                        suppliedBuilder: localBuilderConfig.title,
+                        localAttributes: LabelAttributes(
+                            padding: .padding(
+                                leading: content.icon == nil ? 0 : 6
                             )
                         )
-                    }
-                    .padding(
-                        .padding(
-                            bottom: content.subtitle == nil ? 20 : 0
-                        )
                     )
+                }
+                .padding(
+                    .padding(
+                        bottom: content.subtitle == nil ? 20 : 0
+                    )
+                )
 
-                    withContent(content: content.subtitle) { content in
-                        componentFactory.buildLabel(
-                            config: config.subtitle,
-                            content: content,
-                            suppliedBuilder: localBuilderConfig.subtitle
-                        )
-                    }
+                withContent(content: content.subtitle) { content in
+                    componentFactory.buildLabel(
+                        config: config.subtitle,
+                        content: content,
+                        suppliedBuilder: localBuilderConfig.subtitle
+                    )
                 }
 
-                if usingEmail {
-                    emailPasswordViews
+                if let emailContent = contentObserver.content.emailContent {
+                    AuthenticationEmailPasswordView(
+                        config: config,
+                        content: emailContent,
+                        contentObserver: contentObserver,
+                        componentFactory: componentFactory
+                    )
+                    .environmentObject(navigationState)
                 }
             }
             .padding(style.contentPadding)
@@ -89,110 +110,26 @@ public struct AuthenticationWidget: Container {
         }
         .applyBackground(style.background)
         .padding(style.padding)
-        .onAppear {
-            // When the view appears/reappears trigger a re-validation so that
-            // any changes in field values are reflected in the UI.
-            contentObserver.validateEmailFields()
-        }
-    }
-
-    // MARK: - Internal
-
-    enum Field: Int, Hashable, CaseIterable {
-        case email
-        case password
-    }
-
-    let localBuilderConfig: AuthenticationWidgetBuilderConfig
-    let componentFactory: ComponentFactory
-    @StateObject var contentObserver: ContentObserver
-    let config: ParraAuthConfig
-    let style: AuthenticationWidgetStyle
-
-    @EnvironmentObject var themeObserver: ParraThemeObserver
-
-    var usingEmail: Bool {
-        contentObserver.content.emailContent != nil
-    }
-
-    @ViewBuilder var emailPasswordViews: some View {
-        if let content = contentObserver.content.emailContent {
-            VStack(spacing: 0) {
-                componentFactory.buildTextInput(
-                    config: config.emailField,
-                    content: content.emailField,
-                    localAttributes: TextInputAttributes(
-                        padding: .padding(
-                            top: 50,
-                            bottom: 5
-                        ),
-                        textContentType: .emailAddress
-                    )
+        .navigationDestination(for: String.self) { destination in
+            if destination == "signup" {
+                SignupWidget(
+                    config: config,
+                    style: style,
+                    localBuilderConfig: localBuilderConfig,
+                    componentFactory: componentFactory,
+                    contentObserver: contentObserver
                 )
-                .submitLabel(.next)
-                .focused($focusedField, equals: .email)
-                .onSubmit {
-                    focusNextField($focusedField)
-                }
-
-                componentFactory.buildTextInput(
-                    config: config.passwordField,
-                    content: content.passwordField,
-                    localAttributes: TextInputAttributes(
-                        padding: .padding(bottom: 16),
-                        textContentType: .password
-                    )
-                )
-                .submitLabel(.next)
-                .focused($focusedField, equals: .password)
-                .onSubmit(of: .text) {
-                    contentObserver.loginTapped()
-                }
-
-                componentFactory.buildTextButton(
-                    variant: .contained,
-                    config: config.loginButton,
-                    content: content.loginButton
-                ) {
-                    contentObserver.loginTapped()
-                }
-
-                if let error = contentObserver.error {
-                    componentFactory.buildLabel(
-                        config: config.loginErrorLabel,
-                        content: LabelContent(text: error),
-                        localAttributes: .defaultFormCallout(
-                            in: themeObserver.theme,
-                            with: config.loginErrorLabel,
-                            erroring: true
-                        ).withUpdates(
-                            updates: LabelAttributes(
-                                padding: .padding(top: 4)
-                            )
-                        )
-                    )
-                }
-            }
-
-            Spacer()
-
-            componentFactory.buildTextButton(
-                variant: .plain,
-                config: config.signupButton,
-                content: content.signupButton
-            ) {
-                contentObserver.signupTapped()
+                .edgesIgnoringSafeArea([.top])
+                .environmentObject(navigationState)
             }
         }
     }
 
     // MARK: - Private
 
-    @State private var password: String = ""
-
-    @FocusState private var focusedField: Field?
-
     @Environment(\.parra) private var parra
+
+    @StateObject private var navigationState = NavigationState()
 }
 
 #Preview {
