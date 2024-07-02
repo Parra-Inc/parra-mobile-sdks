ca1b21f16e69ee1af1e86a236b07f6fa3a43a526
Mick MacCallum <micksmaccallum@gmail.com>
2024-07-02 17:20:14 -0400

    improved webview component

--- Sources/Parra/Containers/Views/WebView.swift
+++ Sources/Parra/Containers/Views/WebView.swift
@@ -9,39 +9,122 @@
 import SwiftUI
 import WebKit
 
-struct WebView: UIViewRepresentable {
+struct WebView: View {
     // MARK: - Lifecycle
 
     init(url: URL) {
         self.url = url
-
-        self.webView = WKWebView(frame: .zero)
     }
 
     // MARK: - Internal
 
+    var body: some View {
+        _WebView(
+            url: url,
+            isLoading: $isLoading,
+            errorMessage: $errorMessage
+        )
+        .overlay {
+            if let errorMessage {
+                Text(errorMessage)
+            } else if isLoading {
+                ProgressView()
+            } else {
+                EmptyView()
+            }
+        }
+    }
+
+    // MARK: - Private
+
+    private let url: URL
+    @State private var isLoading: Bool = true
+    @State private var errorMessage: String?
+}
+
+struct _WebView: UIViewRepresentable {
+    // MARK: - Lifecycle
+
+    init(
+        url: URL,
+        isLoading: Binding<Bool>,
+        errorMessage: Binding<String?>
+    ) {
+        Logger.trace("Preparing web view", [
+            "url": url.absoluteString
+        ])
+
+        self.url = url
+        self._isLoading = isLoading
+        self._errorMessage = errorMessage
+    }
+
+    // MARK: - Internal
+
+    class Coordinator: NSObject, WKNavigationDelegate {
+        // MARK: - Lifecycle
+
+        init(_ parent: _WebView) {
+            self.parent = parent
+        }
+
+        // MARK: - Internal
+
+        var parent: _WebView
+
+        func webView(
+            _ webView: WKWebView,
+            didStartProvisionalNavigation navigation: WKNavigation!
+        ) {
+            parent.isLoading = true
+            parent.errorMessage = nil
+
+            Logger.trace("Web view did start navigation")
+        }
+
+        func webView(
+            _ webView: WKWebView,
+            didFinish navigation: WKNavigation!
+        ) {
+            parent.isLoading = false
+
+            Logger.trace("Web view did finish navigation")
+        }
+
+        func webView(
+            _ webView: WKWebView,
+            didFail navigation: WKNavigation!,
+            withError error: Error
+        ) {
+            parent.isLoading = false
+            parent.errorMessage = error.localizedDescription
+
+            Logger.trace("Web view did fail navigation")
+        }
+    }
+
     let url: URL
+    @Binding var isLoading: Bool
+    @Binding var errorMessage: String?
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
 
     func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
+
+        let request = URLRequest(url: url)
+        webView.load(request)
+
         return webView
     }
 
     func updateUIView(
         _ uiView: WKWebView,
         context: Context
-    ) {
-        let request = URLRequest(
-            url: url
-        )
-
-        webView.load(
-            request
-        )
-    }
-
-    // MARK: - Private
-
-    private let webView: WKWebView
+    ) {}
 }
 
 #Preview {
