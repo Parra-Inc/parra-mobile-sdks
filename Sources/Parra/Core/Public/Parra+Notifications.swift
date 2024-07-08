73ab822ff3a369fa8b6a6201aa5154f63646e1b2
Mick MacCallum <micksmaccallum@gmail.com>
2024-07-08 12:42:17 -0400

    comments

--- Sources/Parra/Core/Public/Parra+Notifications.swift
+++ Sources/Parra/Core/Public/Parra+Notifications.swift
@@ -8,27 +8,37 @@
 import Foundation
 
 public extension Parra {
-    /// A notification that is fired with the Parra module is about to begin syncrhonizing data with the Parra API. Each sync
-    /// will include a unique token used to identify a given sync. This token is attached to the notification's `userInfo` with
-    /// the key `Parra.Constant.syncTokenKey`.
+    /// A notification that is fired with the Parra module is about to begin
+    /// syncrhonizing data with the Parra API. Each sync will include a unique
+    /// token used to identify a given sync. This token is attached to the
+    /// notification's `userInfo` with the key `Parra.Constant.syncTokenKey`.
     static let syncDidBeginNotification = NSNotification
         .Name("ParraSyncDidBeginNotification")
 
-    /// A notification that is fired with the Parra module is about has completed syncrhonizing data with the Parra API. Each sync
-    /// will include a unique token used to identify a given sync. This token is attached to the notification's `userInfo` with
-    /// the key `Parra.Constant.syncTokenKey`.
+    /// A notification that is fired with the Parra module is about has
+    /// completed syncrhonizing data with the Parra API. Each sync will include
+    /// a unique token used to identify a given sync. This token is attached to
+    /// the notification's `userInfo` with the key `Parra.Constant.syncTokenKey`.
     static let syncDidEndNotification = NSNotification
         .Name("ParraSyncDidEndNotification")
 
-    /// A notification that is fired when the Parra theme is about to change. At this point, no UI elements have been updated to
+    /// A notification that is fired when the Parra theme is about to change.
+    /// At this point, no UI elements have been updated to
     /// reflect the new theme.
     static let themeWillChangeNotification = NSNotification
         .Name("ParraThemeWillChangeNotification")
 
-    /// A notification that is fired when the Parra theme has just changed. At this point, all Parra UI elements have been updated to
+    /// A notification that is fired when the Parra theme has just changed.
+    /// At this point, all Parra UI elements have been updated to
     /// reflect the new theme.
     static let themeDidChangeNotification = NSNotification
         .Name("ParraThemeDidChangeNotification")
+
+    /// A notification that is fired when the user's preferred appearance has
+    /// just changed. At this point, all Parra UI elements have been updated to
+    /// reflect the new selection.
+    static let preferredAppearanceDidChangeNotification = NSNotification
+        .Name("ParrapreferredAppearanceDidChangeNotification")
 
     static let authenticationStateDidChangeNotification = NSNotification
         .Name("ParraAuthenticationStateDidChangeNotification")
