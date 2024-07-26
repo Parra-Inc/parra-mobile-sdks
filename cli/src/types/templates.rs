680727c08e80b411c22cf65253bf4d76d83ed5d1
Mick MacCallum <micksmaccallum@gmail.com>
2024-07-26 11:52:54 -0400

    types for template

--- cli/src/types/templates.rs
+++ cli/src/types/templates.rs
@@ -7,35 +7,59 @@ pub struct AppNameInfo {
     pub upper_camel: String,
 }
 
+// #[derive(Debug, Deserialize, Clone, Serialize)]
+// pub struct AppEntitlementInfo {
+//     pub aps_environment: String,
+//     pub associated_domains: String,
+// }
+
+// #[derive(Debug, Deserialize, Clone, Serialize)]
+// pub struct AppEntitlementSchemes {
+//     pub debug: AppEntitlementInfo,
+//     pub release: AppEntitlementInfo,
+// }
+
 #[derive(Debug, Deserialize, Clone, Serialize)]
-pub struct AppEntitlementInfo {
-    pub aps_environment: String,
-    pub associated_domains: String,
+pub struct CodeSigningConfig {
+    pub identity: String,
+    pub required: String,
+    pub allowed: String,
 }
 
 #[derive(Debug, Deserialize, Clone, Serialize)]
-pub struct AppEntitlementSchemes {
-    pub debug: AppEntitlementInfo,
-    pub release: AppEntitlementInfo,
+pub struct CodeSigningConfigs {
+    pub debug: CodeSigningConfig,
+    pub release: CodeSigningConfig,
 }
 
 #[derive(Debug, Deserialize, Clone, Serialize)]
-pub struct AppTemplateInfo {
-    pub id: String,
+pub struct AppContextInfo {
     pub name: AppNameInfo,
     pub bundle_id: String,
     pub deployment_target: String,
-    pub entitlements: AppEntitlementSchemes,
+    // pub entitlements: AppEntitlementSchemes,
+    pub code_sign: CodeSigningConfigs,
+    // team id or "-"
+    pub team_id: String,
 }
 
-#[derive(Debug, Deserialize, Clone, Serialize)]
-pub struct TenantTemplateInfo {
-    pub id: String,
-    pub name: String,
-}
+// #[derive(Debug, Deserialize, Clone, Serialize)]
+// pub struct TenantContextInfo {
+//     pub id: String,
+//     pub name: String,
+// }
 
 #[derive(Debug, Deserialize, Clone, Serialize)]
-pub struct ProjectTemplate {
-    pub app: AppTemplateInfo,
-    pub tenant: TenantTemplateInfo,
+pub struct SdkContextInfo {
+    pub version: String,
+}
+
+/// In order to be able to effectively recycle logic between the sample app
+/// and project generator, this object needs to contain a union of all fields
+/// required by both these use cases.
+#[derive(Debug, Deserialize, Clone, Serialize)]
+pub struct ProjectContext {
+    pub app: AppContextInfo,
+    // pub tenant: TenantContextInfo,
+    pub sdk: SdkContextInfo,
 }
