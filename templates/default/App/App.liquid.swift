1a73b7b21e0ee3f0dbd43273b209b43e78d30bce
Mick MacCallum <micksmaccallum@gmail.com>
2024-07-29 14:31:57 -0400

    yep

--- templates/default/App/App.liquid.swift
+++ templates/default/App/App.liquid.swift
@@ -1,6 +1,6 @@
 //
 //  {{ app.name.upper_camel }}App.swift
-//  {{ app.name }}
+//  {{ app.name.raw }}
 //
 //  Bootstrapped with ❤️ by Parra on {{ "now" | date: "%m/%d/%Y" }}.
 //  Copyright © {{ "now" | date: "%Y" }} {{ tenant.name }}. All rights reserved.
