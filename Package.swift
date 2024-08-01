e8df7f22a05cfcda3fc021595333a3a175c30ae8
Mick MacCallum <micksmaccallum@gmail.com>
2024-08-01 10:35:24 -0400

    Restructure project for SPM

--- Package.swift
+++ Package.swift
@@ -3,6 +3,8 @@
 
 import PackageDescription
 
+let iosSdkDir = "ios"
+
 let package = Package(
     name: "Parra",
     defaultLocalization: "en",
@@ -13,16 +15,23 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Parra",
-            targets: ["Parra"]),
+            targets: [
+                "Parra"
+            ]
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Parra"),
+            name: "Parra",
+            path: "\(iosSdkDir)/Sources/Parra"
+        ),
         .testTarget(
             name: "ParraTests",
-            dependencies: ["Parra"]),
+            dependencies: ["Parra"],
+            path: "\(iosSdkDir)/Tests"
+        ),
     ],
     swiftLanguageVersions: [.version("5.9")]
 )
