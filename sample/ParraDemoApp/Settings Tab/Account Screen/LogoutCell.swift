55adf57d22d9a8be3b9bc5ad88ecfcf2610f2685
Mick MacCallum <micksmaccallum@gmail.com>
2024-08-14 01:42:13 +0000

    Released version 0.2.7 [skip ci]

--- sample/ParraDemoApp/Settings Tab/Account Screen/LogoutCell.swift
+++ sample/ParraDemoApp/Settings Tab/Account Screen/LogoutCell.swift
@@ -2,7 +2,7 @@
 //  LogoutCell.swift
 //  Parra Demo
 //
-//  Bootstrapped with ❤️ by Parra on 08/13/2024.
+//  Bootstrapped with ❤️ by Parra on 08/14/2024.
 //  Copyright © 2024 Parra Inc.. All rights reserved.
 //
 
