f1bae8b70a460d4e9c4b0f91eab73e49ce489ed3
Mick MacCallum <micksmaccallum@gmail.com>
2024-08-20 20:02:35 +0000

    Released version 0.2.28 [skip ci]

--- sample/ParraDemoApp/Settings Tab/Account Screen/DeleteAccountCell.swift
+++ sample/ParraDemoApp/Settings Tab/Account Screen/DeleteAccountCell.swift
@@ -2,7 +2,7 @@
 //  DeleteAccountCell.swift
 //  Parra Demo
 //
-//  Bootstrapped with ❤️ by Parra on 08/19/2024.
+//  Bootstrapped with ❤️ by Parra on 08/20/2024.
 //  Copyright © 2024 Parra Inc.. All rights reserved.
 //
 
