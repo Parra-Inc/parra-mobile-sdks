ecb69f24e245ebf1632a5b3baa21cc4eb5f6926a
Mick MacCallum <micksmaccallum@gmail.com>
2024-08-24 16:15:19 -0400

    Pop after deleting account in optional auth

--- dev/ParraDev/Settings Tab/Account Screen/DeleteAccountCell.swift
+++ dev/ParraDev/Settings Tab/Account Screen/DeleteAccountCell.swift
@@ -12,10 +12,19 @@ import SwiftUI
 struct DeleteAccountCell: View {
     @Environment(\.parra) private var parra
     @Environment(\.parraTheme) private var parraTheme
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
 
     var body: some View {
         Button(action: {
-            parra.auth.deleteAccount()
+            Task {
+                do {
+                    try await parra.auth.deleteAccount()
+
+                    presentationMode.wrappedValue.dismiss()
+                } catch {
+                    ParraLogger.error("Error deleting account", error)
+                }
+            }
         }) {
             Label(
                 title: { Text("Delete Account") },
