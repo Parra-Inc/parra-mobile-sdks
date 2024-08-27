267d3d916ce59338732bf3d1337abef9020ea93b
Mick MacCallum <micksmaccallum@gmail.com>
2024-08-27 12:57:44 +0000

    Released version 0.2.37 [skip ci]

--- sample/ParraDemoApp/Settings Tab/Cells/ShareCell.swift
+++ sample/ParraDemoApp/Settings Tab/Cells/ShareCell.swift
@@ -2,7 +2,7 @@
 //  ShareCell.swift
 //  Parra Demo
 //
-//  Bootstrapped with ❤️ by Parra on 08/26/2024.
+//  Bootstrapped with ❤️ by Parra on 08/27/2024.
 //  Copyright © 2024 Parra Inc.. All rights reserved.
 //
 
