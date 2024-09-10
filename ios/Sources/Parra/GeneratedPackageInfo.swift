7818833d898f7011fc3a878e0ece1c52e1679026
Mick MacCallum <micksmaccallum@gmail.com>
2024-09-10 20:03:38 +0000

    Released version 0.3.8 [skip ci]

--- ios/Sources/Parra/GeneratedPackageInfo.swift
+++ ios/Sources/Parra/GeneratedPackageInfo.swift
@@ -1,2 +1,2 @@
-enum ParraPackageInfo { static let version = "0.3.6" }
+enum ParraPackageInfo { static let version = "0.3.8" }
 
--- sample/ParraDemoApp/Settings Tab/SettingsTab.swift
+++ sample/ParraDemoApp/Settings Tab/SettingsTab.swift
@@ -2,7 +2,7 @@
 //  SettingsTab.swift
 //  Parra Demo
 //
-//  Bootstrapped with ❤️ by Parra on 09/09/2024.
+//  Bootstrapped with ❤️ by Parra on 09/10/2024.
 //  Copyright © 2024 Parra Inc.. All rights reserved.
 //
 
