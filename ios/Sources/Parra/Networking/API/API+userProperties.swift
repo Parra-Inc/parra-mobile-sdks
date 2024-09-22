6a9d4e1871226b862e34331d3875083fadf8646f
Mick MacCallum <micksmaccallum@gmail.com>
2024-09-22 10:52:43 -0400

    wip endpoint

---
+++ ios/Sources/Parra/Networking/API/API+userProperties.swift
@@ -0,0 +1,26 @@
+//
+//  API+userProperties.swift
+//
+//
+//  Created by Mick MacCallum on 9/22/24.
+//
+
+import Foundation
+
+private let logger = Logger()
+
+extension API {
+    func getUserProperties(
+    ) async throws -> ParraImageAssetStub {
+//        let imageField = try MultipartFormField(
+//            name: "image", // server side requirement
+//            image: image
+//        )
+//
+//        return try await hitUploadEndpoint(
+//            .postUpdateAvatar,
+//            cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
+//            formFields: [imageField]
+//        )
+    }
+}
