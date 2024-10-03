c48c34997caf5d7d90f3b7a33b72f4e0e6bff0fe
Mick MacCallum <micksmaccallum@gmail.com>
2024-10-03 13:52:13 +0000

    Released version 0.5.12 [skip ci]

--- sample/ParraDemoApp/ContentView.swift
+++ sample/ParraDemoApp/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  Parra Demo
 //
-//  Bootstrapped with ❤️ by Parra on 10/02/2024.
+//  Bootstrapped with ❤️ by Parra on 10/03/2024.
 //  Copyright © 2024 Parra Inc.. All rights reserved.
 //
 
