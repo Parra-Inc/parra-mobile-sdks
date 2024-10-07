4aa5ef85acb6a470cea7f95e6b23ae15deaf2083
Mick MacCallum <micksmaccallum@gmail.com>
2024-10-07 09:18:11 -0400

    test

--- ios/Sources/ParraStorefront/ParraStorefrontWidget.swift
+++ ios/Sources/ParraStorefront/ParraStorefrontWidget.swift
@@ -5,23 +5,71 @@
 //  Created by Mick MacCallum on 10/6/24.
 //
 
+import Buy
 import Parra
+import ShopifyCheckoutSheetKit
 import SwiftUI
 
 public struct ParraStorefrontWidget: View {
     // MARK: - Lifecycle
 
-    public init() {}
+    public init() {
+        let client = Graph.Client(
+            shopDomain: "",
+            apiKey: ""
+        )
+
+        client.cachePolicy = .cacheFirst(expireIn: 3_600)
+        self.client = client
+    }
 
     // MARK: - Public
 
     public var body: some View {
         Text("Hello, World!")
         Text(parraAuthState.user?.info.displayName ?? "idk")
+
+        Button("Store stuff") {
+            _Concurrency.Task {
+                do {
+                    let q = Storefront.buildQuery { $0
+                        .product { $0
+                        }
+                    }
+                    try await performQuery(q)
+                } catch {}
+            }
+        }
     }
 
     // MARK: - Internal
 
+    let client: Graph.Client
+
     @Environment(\.parra) var parra
     @Environment(\.parraAuthState) var parraAuthState
+
+    func performQuery(
+        _ query: Storefront.QueryRootQuery
+    ) async throws -> Storefront.QueryRoot {
+        return try await withCheckedThrowingContinuation { continuation in
+            let task = client.queryGraphWith(query) { response, error in
+                if let error {
+                    continuation.resume(
+                        throwing: error
+                    )
+                } else if let response {
+                    continuation.resume(
+                        returning: response
+                    )
+                } else {
+//                    continuation.resume(
+//                        throwing:
+//                    )
+                }
+            }
+
+            task.resume()
+        }
+    }
 }
