523e6500b9c2ace709491f6edf0fda0f9f41599f
Mick MacCallum <micksmaccallum@gmail.com>
2024-10-28 09:35:27 -0400

    Fixed missing fixture issues

---
+++ ios/Sources/Parra/Containers/Widgets/Feed/Fixtures/ParraCreatorUpdateAppStub+Fixtures.swift
@@ -0,0 +1,71 @@
+//
+//  ParraCreatorUpdateAppStub+Fixtures.swift
+//  Parra
+//
+//  Created by Mick MacCallum on 10/26/24.
+//
+
+import SwiftUI
+
+extension ParraCreatorUpdateAppStub: ParraFixture {
+    public static func validStates() -> [ParraCreatorUpdateAppStub] {
+        return [
+            ParraCreatorUpdateAppStub(
+                id: .uuid,
+                createdAt: .now,
+                updatedAt: .now,
+                deletedAt: nil,
+                title: "Week 8 Picks",
+                body: "I couldn't believe we landed so many great picks last week. This week is going to be wild.",
+                sender: ParraCreatorUpdateSenderStub(
+                    id: .uuid,
+                    name: "Mick M.",
+                    avatar: ParraImageAssetStub(
+                        id: .uuid,
+                        size: CGSize(width: 4_284, height: 4_284),
+                        url: URL(
+                            string: "https://image-asset-bucket-production.s3.amazonaws.com/users/9a311ff4-6af9-445e-a3df-6900787e2198/avatar/075b05ea-999d-49b9-a48e-0e5bcc427e44.jpg"
+                        )!
+                    ),
+                    verified: true
+                ),
+                attachments: [
+                    ParraCreatorUpdateAttachmentStub(
+                        id: .uuid,
+                        image: ParraImageAssetStub(
+                            id: .uuid,
+                            size: CGSize(width: 660, height: 300),
+                            url: URL(
+                                string: "https://image-asset-bucket-production.s3.amazonaws.com/tenants/0c6583c7-a1ed-4c8e-8373-d728986e6d53/creator-update-attachment/af444818-5f4b-4b14-94ff-14153a1daa55.png"
+                            )!
+                        )
+                    ),
+                    ParraCreatorUpdateAttachmentStub(
+                        id: .uuid,
+                        image: ParraImageAssetStub(
+                            id: .uuid,
+                            size: CGSize(width: 560, height: 560),
+                            url: URL(
+                                string: "https://image-asset-bucket-production.s3.amazonaws.com/tenants/0c6583c7-a1ed-4c8e-8373-d728986e6d53/creator-update-attachment/ddf7d09e-5e3c-452e-ab65-8504b68de604.jpg"
+                            )!
+                        )
+                    ),
+                    ParraCreatorUpdateAttachmentStub(
+                        id: .uuid,
+                        image: ParraImageAssetStub(
+                            id: .uuid,
+                            size: CGSize(width: 1_320, height: 420),
+                            url: URL(
+                                string: "https://image-asset-bucket-production.s3.amazonaws.com/tenants/0c6583c7-a1ed-4c8e-8373-d728986e6d53/creator-update-attachment/c7bab6d2-cb1d-46c7-9f5b-71753afa465a.png"
+                            )!
+                        )
+                    )
+                ]
+            )
+        ]
+    }
+
+    public static func invalidStates() -> [ParraCreatorUpdateAppStub] {
+        return []
+    }
+}
---
+++ ios/Sources/Parra/Containers/Widgets/Feed/Fixtures/ParraCreatorUpdateAttachmentStub+Fixtures.swift
@@ -0,0 +1,69 @@
+//
+//  ParraCreatorUpdateAttachmentStub+Fixtures.swift
+//  Parra
+//
+//  Created by Mick MacCallum on 10/27/24.
+//
+
+import SwiftUI
+
+extension ParraCreatorUpdateAttachmentStub: ParraFixture {
+    public static func validStates() -> [ParraCreatorUpdateAttachmentStub] {
+        return [
+            ParraCreatorUpdateAttachmentStub(
+                id: .uuid,
+                image: ParraImageAssetStub(
+                    id: .uuid,
+                    size: CGSize(width: 4_284, height: 4_284),
+                    url: URL(
+                        string: "https://image-asset-bucket-production.s3.amazonaws.com/users/9a311ff4-6af9-445e-a3df-6900787e2198/avatar/075b05ea-999d-49b9-a48e-0e5bcc427e44.jpg"
+                    )!
+                )
+            ),
+            ParraCreatorUpdateAttachmentStub(
+                id: .uuid,
+                image: ParraImageAssetStub(
+                    id: .uuid,
+                    size: CGSize(width: 600, height: 300),
+                    url: URL(
+                        string: "https://image-asset-bucket-production.s3.amazonaws.com/tenants/0c6583c7-a1ed-4c8e-8373-d728986e6d53/creator-update-attachment/af444818-5f4b-4b14-94ff-14153a1daa55.png"
+                    )!
+                )
+            ),
+            ParraCreatorUpdateAttachmentStub(
+                id: .uuid,
+                image: ParraImageAssetStub(
+                    id: .uuid,
+                    size: CGSize(width: 1_320, height: 420),
+                    url: URL(
+                        string: "https://image-asset-bucket-production.s3.amazonaws.com/tenants/0c6583c7-a1ed-4c8e-8373-d728986e6d53/creator-update-attachment/c7bab6d2-cb1d-46c7-9f5b-71753afa465a.png"
+                    )!
+                )
+            ),
+            ParraCreatorUpdateAttachmentStub(
+                id: .uuid,
+                image: ParraImageAssetStub(
+                    id: .uuid,
+                    size: CGSize(width: 2_940, height: 1_960),
+                    url: URL(
+                        string: "https://plus.unsplash.com/premium_photo-1695635984394-79e2f6e79f92?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    )!
+                )
+            ),
+            ParraCreatorUpdateAttachmentStub(
+                id: .uuid,
+                image: ParraImageAssetStub(
+                    id: .uuid,
+                    size: CGSize(width: 2_864, height: 3_580),
+                    url: URL(
+                        string: "https://images.unsplash.com/photo-1728875650224-fd3f375f6546?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    )!
+                )
+            )
+        ]
+    }
+
+    public static func invalidStates() -> [ParraCreatorUpdateAttachmentStub] {
+        return []
+    }
+}
