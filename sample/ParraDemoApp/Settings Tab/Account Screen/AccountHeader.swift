b437c1d0a1f8e9f8297daf9fe9fc6359368b7196
Mick MacCallum <micksmaccallum@gmail.com>
2024-11-05 02:19:54 +0000

    Released version 0.14.4 [skip ci]

--- sample/ParraDemoApp/Settings Tab/Account Screen/AccountHeader.swift
+++ sample/ParraDemoApp/Settings Tab/Account Screen/AccountHeader.swift
@@ -2,7 +2,7 @@
 //  AccountHeader.swift
 //  Parra Demo
 //
-//  Bootstrapped with ❤️ by Parra on 11/04/2024.
+//  Bootstrapped with ❤️ by Parra on 11/05/2024.
 //  Copyright © 2024 Parra Inc.. All rights reserved.
 //
 
