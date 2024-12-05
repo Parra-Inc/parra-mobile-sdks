2d0cd9b6e1cda344efe5165b459f74e8c4b86b0c
Mick MacCallum <micksmaccallum@gmail.com>
2024-12-05 10:20:05 -0500

    added isPreview check

--- ios/Sources/Parra/Sessions/Extensions/UIDevice+modelCode.swift
+++ ios/Sources/Parra/Sessions/Extensions/UIDevice+modelCode.swift
@@ -11,13 +11,13 @@ import UIKit
 
 extension UIDevice {
     static var modelCode: String {
-        if let simulatorModelIdentifier = ProcessInfo()
-            .environment["SIMULATOR_MODEL_IDENTIFIER"]
-        {
+        if let simulatorModelIdentifier = ProcessInfo.processInfo.environment[
+            "SIMULATOR_MODEL_IDENTIFIER"
+        ] {
             return "Simulator \(simulatorModelIdentifier)"
         }
-        var systemInfo = utsname()
 
+        var systemInfo = utsname()
         uname(&systemInfo)
 
         return withUnsafeMutablePointer(to: &systemInfo.machine) {
@@ -28,4 +28,18 @@ extension UIDevice {
             )
         }
     }
+
+    static let isPreview: Bool = {
+        #if targetEnvironment(simulator)
+        guard let flag = ProcessInfo.processInfo.environment[
+            "XCODE_RUNNING_FOR_PREVIEWS"
+        ] else {
+            return false
+        }
+
+        return flag == "1"
+        #else
+        return false
+        #endif
+    }()
 }
