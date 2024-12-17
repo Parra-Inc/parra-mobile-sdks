85822c294ff2ce29e3e8ee115e64adf89c60a8a0
Mick MacCallum <micksmaccallum@gmail.com>
2024-12-17 02:41:45 +0000

    Released version 0.27.0 [skip ci]

--- sample/ParraDemoApp/Settings Tab/Edit Profile Screen/EditProfileViewModel.swift
+++ sample/ParraDemoApp/Settings Tab/Edit Profile Screen/EditProfileViewModel.swift
@@ -2,7 +2,7 @@
 //  EditProfileViewModel.swift
 //  Parra Demo
 //
-//  Bootstrapped with ❤️ by Parra on 12/14/2024.
+//  Bootstrapped with ❤️ by Parra on 12/17/2024.
 //  Copyright © 2024 Parra Inc.. All rights reserved.
 //
 
