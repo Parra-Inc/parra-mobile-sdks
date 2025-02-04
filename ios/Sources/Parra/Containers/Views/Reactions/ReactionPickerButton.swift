d3ca753b2bb1acc99bca2f9e370d9129cf7c3b4e
Mick MacCallum <micksmaccallum@gmail.com>
2025-02-04 14:09:20 -0500

    haptic feedback and animation on reaction picker modal

---
+++ ios/Sources/Parra/Containers/Views/Reactions/ReactionPickerButton.swift
@@ -0,0 +1,119 @@
+//
+//  ReactionPickerButton.swift
+//  Parra
+//
+//  Created by Mick MacCallum on 2/4/25.
+//
+
+import SwiftUI
+
+struct ReactionPickerButton: View {
+    // MARK: - Lifecycle
+
+    init(
+        reactor: StateObject<Reactor>,
+        reactionOption: ParraReactionOption,
+        showLabels: Bool
+    ) {
+        self._reactor = reactor
+        self.reactionOption = reactionOption
+        self.showLabels = showLabels
+    }
+
+    // MARK: - Internal
+
+    var body: some View {
+        Button {
+            toggleAnimation = true
+
+            reactor.addNewReaction(option: reactionOption)
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                toggleAnimation = false
+            }
+        } label: {
+            VStack {
+                RoundedRectangle(cornerRadius: 16)
+                    .fill((
+                        reactor.currentActiveReactionIds
+                            .contains(reactionOption.id) ? .blue :
+                            Color
+                            .gray
+                    ).opacity(0.4))
+                    .frame(width: 64, height: 64)
+                    .overlay {
+                        reactionContent(for: reactionOption)
+                            .scaleEffect(toggleAnimation ? 1.35 : 1.0)
+                            .animation(toggleAnimation ? Animation.easeInOut(
+                                duration: 0.1
+                            ) : Animation.easeInOut, value: toggleAnimation)
+                    }
+
+                if showLabels {
+                    Text(reactionOption.name)
+                        .font(.caption2)
+                        .multilineTextAlignment(.center)
+                        .lineLimit(2, reservesSpace: true)
+                        .backgroundStyle(.red)
+                }
+            }
+        }
+        // Required to prevent highlighting the button then dragging the scroll
+        // view from causing the button to be pressed.
+        .simultaneousGesture(TapGesture())
+        .buttonStyle(.plain)
+        .sensoryFeedback(.impact, trigger: toggleAnimation)
+    }
+
+    // MARK: - Private
+
+    @State private var toggleAnimation: Bool = .init()
+
+    private var reactionOption: ParraReactionOption
+    private var showLabels: Bool
+    @StateObject private var reactor: Reactor
+
+    @Environment(\.parraComponentFactory) private var componentFactory
+
+    @ViewBuilder
+    private func reactionContent(
+        for reactionOption: ParraReactionOption
+    ) -> some View {
+        switch reactionOption.type {
+        case .custom:
+            if let url = URL(string: reactionOption.value) {
+                if url.lastPathComponent.hasSuffix(".gif") {
+                    GifImageView(url: url)
+                        .frame(
+                            width: 40.0,
+                            height: 40.0
+                        )
+                } else {
+                    componentFactory.buildAsyncImage(
+                        config: ParraAsyncImageConfig(
+                            aspectRatio: 1.0,
+                            contentMode: .fit
+                        ),
+                        content: ParraAsyncImageContent(
+                            url: url
+                        ),
+                        localAttributes: ParraAttributes.AsyncImage(
+                            size: CGSize(
+                                width: 40.0,
+                                height: 40.0
+                            )
+                        )
+                    )
+                }
+            }
+        case .emoji:
+            Text(reactionOption.value)
+                .font(.system(size: 500))
+                .minimumScaleFactor(0.01)
+                .frame(
+                    width: 40.0,
+                    height: 40.0
+                )
+        }
+    }
+}
--- ios/Sources/Parra/Containers/Views/Reactions/ReactionPickerGridView.swift
+++ ios/Sources/Parra/Containers/Views/Reactions/ReactionPickerGridView.swift
@@ -31,35 +31,11 @@ struct ReactionPickerGridView: View {
             ForEach(searchResults, id: \.self) { group in
                 Section {
                     ForEach(group.options, id: \.self) { reactionOption in
-                        Button {
-                            reactor.addNewReaction(option: reactionOption)
-                        } label: {
-                            VStack {
-                                RoundedRectangle(cornerRadius: 16)
-                                    .fill((
-                                        reactor.currentActiveReactionIds
-                                            .contains(reactionOption.id) ? .blue :
-                                            Color
-                                            .gray
-                                    ).opacity(0.4))
-                                    .frame(width: 64, height: 64)
-                                    .overlay {
-                                        reactionContent(for: reactionOption)
-                                    }
-
-                                if showLabels {
-                                    Text(reactionOption.name)
-                                        .font(.caption2)
-                                        .multilineTextAlignment(.center)
-                                        .lineLimit(2, reservesSpace: true)
-                                        .backgroundStyle(.red)
-                                }
-                            }
-                        }
-                        // Required to prevent highlighting the button then dragging the scroll
-                        // view from causing the button to be pressed.
-                        .simultaneousGesture(TapGesture())
-                        .buttonStyle(.plain)
+                        ReactionPickerButton(
+                            reactor: _reactor,
+                            reactionOption: reactionOption,
+                            showLabels: showLabels
+                        )
                     }
                 } header: {
                     VStack {
@@ -88,46 +64,4 @@ struct ReactionPickerGridView: View {
     private var showLabels: Bool
 
     @Environment(\.parraComponentFactory) private var componentFactory
-
-    @ViewBuilder
-    private func reactionContent(
-        for reactionOption: ParraReactionOption
-    ) -> some View {
-        switch reactionOption.type {
-        case .custom:
-            if let url = URL(string: reactionOption.value) {
-                if url.lastPathComponent.hasSuffix(".gif") {
-                    GifImageView(url: url)
-                        .frame(
-                            width: 40.0,
-                            height: 40.0
-                        )
-                } else {
-                    componentFactory.buildAsyncImage(
-                        config: ParraAsyncImageConfig(
-                            aspectRatio: 1.0,
-                            contentMode: .fit
-                        ),
-                        content: ParraAsyncImageContent(
-                            url: url
-                        ),
-                        localAttributes: ParraAttributes.AsyncImage(
-                            size: CGSize(
-                                width: 40.0,
-                                height: 40.0
-                            )
-                        )
-                    )
-                }
-            }
-        case .emoji:
-            Text(reactionOption.value)
-                .font(.system(size: 500))
-                .minimumScaleFactor(0.01)
-                .frame(
-                    width: 40.0,
-                    height: 40.0
-                )
-        }
-    }
 }
