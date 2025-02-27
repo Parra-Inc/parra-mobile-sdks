2c75f36b13f8b3c7d2a0b6655c035ebcb09d5280
Mick MacCallum <micksmaccallum@gmail.com>
2025-02-27 14:18:47 -0500

    Update template to reflect new presentation bindings

--- templates/default/App/Settings Tab/ListItemLoadingButton.liquid.swift
+++ templates/default/App/Settings Tab/ListItemLoadingButton.liquid.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import Parra
 
 struct ListItemLabel: View {
     let text: String
@@ -42,13 +43,17 @@ struct ListItemLabel: View {
 }
 
 struct ListItemLoadingButton: View {
-    @Binding var isLoading: Bool
+    @Binding var presentationState: ParraSheetPresentationState
     let text: String
     let symbol: String
 
+    private var isLoading: Bool {
+        return presentationState == .loading
+    }
+
     var body: some View {
         Button {
-            isLoading = true
+            presentationState = .loading
         } label: {
             ListItemLabel(
                 text: text,
