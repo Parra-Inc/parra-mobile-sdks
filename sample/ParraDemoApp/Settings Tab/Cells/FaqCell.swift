c6c621dbd076cc2595db9f1b1ebf716e46bb08cd
Mick MacCallum <micksmaccallum@gmail.com>
2025-05-27 17:53:33 +0000

    Released version 0.32.3 [skip ci]

--- sample/ParraDemoApp/Settings Tab/Cells/FaqCell.swift
+++ sample/ParraDemoApp/Settings Tab/Cells/FaqCell.swift
@@ -2,7 +2,7 @@
 //  FaqCell.swift
 //  Parra Demo
 //
-//  Bootstrapped with ❤️ by Parra on 05/21/2025.
+//  Bootstrapped with ❤️ by Parra on 05/27/2025.
 //  Copyright © 2025 Parra Inc.. All rights reserved.
 //
 
