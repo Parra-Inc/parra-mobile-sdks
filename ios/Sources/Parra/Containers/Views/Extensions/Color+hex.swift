36eb6ade232aa9100d0ade7728118a692cbb60d8
Mick MacCallum <micksmaccallum@gmail.com>
2025-06-02 14:15:58 -0400

    added public color from hex init for cli

--- ios/Sources/Parra/Containers/Views/Extensions/Color+hex.swift
+++ ios/Sources/Parra/Containers/Views/Extensions/Color+hex.swift
@@ -40,6 +40,57 @@ extension Color {
         )
     }
 
+    /// Initializes a String from a hexadecimal string representation.
+    ///
+    /// - Parameter hex: The hexadecimal string to convert.
+    /// - Returns: A new String initialized from the hexadecimal representation,
+    ///            or an empty string if the input is not a valid hex string.
+    public static func fromHex(_ hex: String) -> Color {
+        var cString: String = hex.trimmingCharacters(
+            in: .whitespacesAndNewlines
+        ).uppercased()
+
+        if cString.hasPrefix("#") {
+            cString.remove(at: cString.startIndex)
+        }
+
+        // Ensure the hex string has an even number of characters
+        guard hex.count % 2 == 0 else {
+            return .black
+        }
+
+        let r, g, b, a: UInt64
+
+        let start = hex.index(hex.startIndex, offsetBy: 1)
+        let hexColor = String(hex[start...])
+
+        if hexColor.count >= 6 {
+            let scanner = Scanner(string: hexColor)
+            var hexNumber: UInt64 = 0
+
+            if scanner.scanHexInt64(&hexNumber) {
+                r = hexNumber & 0xFF00_0000 >> 24
+                g = hexNumber & 0x00FF_0000 >> 16
+                b = hexNumber & 0x0000_FF00 >> 8
+
+                if hexColor.count > 6 {
+                    a = hexNumber & 0x0000_00FF
+                } else {
+                    a = 255
+                }
+
+                return Color(
+                    red: Int(r),
+                    green: Int(g),
+                    blue: Int(b),
+                    opacity: CGFloat(a) / 255
+                )
+            }
+        }
+
+        return .black
+    }
+
     func hex(
         withHashPrefix: Bool = true
     ) -> String {
